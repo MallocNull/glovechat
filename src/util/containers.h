7f4bf44f58f571bd951140e134575cdae9153952
malloc <aleco@aroltd.com>
2019-02-14 15:27:51 -0600

    no look

--- src/util/containers.h
+++ src/util/containers.h
@@ -2,9 +2,21 @@
 #define GLV_UTIL_CONTAINERS_H
 
 typedef struct {
+    void* key;
+    void* value;
+} glv_pair_t;
 
+typedef struct {
+    glv_pair_t** buckets;
+    int* bucket_lengths;
+    int bucket_count;
+    int pair_count;
 } glv_map_t;
 
 glv_map_t* glv_map_create();
+glv_map_t* glv_map_create_ex(int initial_size);
+
+void glv_map_destroy(glv_map_t* map);
+void glv_map_destroy_dealloc(glv_map_t* map);
 
 #endif
